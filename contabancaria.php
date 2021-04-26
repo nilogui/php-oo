3b64136de550662b0f749f80bcd5579d10339122
Nilogui <nilogui@gmail.com>
2021-04-26 14:34:00 -0300

    Classes e Propriedades

--- contabancaria.php
+++ contabancaria.php
@@ -2,13 +2,20 @@
 
 class ContaBancaria
 {
-
-
+    // public(aberto) - private(fechado) - protected(acessar por exceção. É semelhante ao private)
+    public $banco;
+    public $nomeTitular = 'Nilo Guimarães';
+    public $numeroAgencia = '3467';
+    public $numeroConta;
+    public $saldo = 1000.00;   
 }
-
 
 $conta = new ContaBancaria();
 
-var_dump($conta);
+//var_dump($conta);
+var_dump($conta->nomeTitular);
+var_dump($conta->numeroAgencia);
 
+$conta->numeroAgencia ='12345'; var_dump($conta->numeroAgencia);
 
+var_dump($conta->saldo); $conta->saldo = 0.00; var_dump($conta->saldo);
